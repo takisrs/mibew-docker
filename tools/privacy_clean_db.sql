0d103c9a5be31340dfdbed382d71165547768f5c
Tobias Kurze <it@t-kurze.de>
2022-02-10 17:11:57 +0100

    added privacy_clean_db.sql to clean db of sensitive data

---
+++ tools/privacy_clean_db.sql
@@ -0,0 +1,2 @@
+DELETE FROM thread t  WHERE t.dtmmodified <= UNIX_TIMESTAMP() - 14*24*60*60;  
+UPDATE thread t SET t.remote = CONCAT(REGEXP_SUBSTR(t.remote, "(^[0-9]{1,3}.[0-9]{1,3})", 1, 1), ".?") WHERE t.dtmmodified <= UNIX_TIMESTAMP() - 3*24*60*60;
